8281258b41d53349f4c60e89492c45ad3d8fb0ef
rusefillc <sdfsdfqsf2334234234>
2021-04-20 12:16:55 -0400

    rev 'b' time

--- step2_copy_hellen64_na6.sh
+++ step2_copy_hellen64_na6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd hellen-one
-python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "64_miataNA6_94" "a"
+python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "64_miataNA6_94" "b"
 
 echo "Done!"
