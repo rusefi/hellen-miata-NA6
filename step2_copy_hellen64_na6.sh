71c8cce6a1e1273e8a474768ccd534fe1441d84f
andreika-git <andrei.home@gmail.com>
2022-08-24 16:56:03 +0300

    rev.D with mcu-144

--- step2_copy_hellen64_na6.sh
+++ step2_copy_hellen64_na6.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd hellen-one
-python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "64_NA6_94" "c"
+python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "64_NA6_94" "d"
 
 echo "Done!"
--- step3_create_hellen64_na6.sh
+++ step3_create_hellen64_na6.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd hellen-one
-sh bin/create_board.sh ".." "64_NA6_94" "c" "bom_replace_64_NA6_94.csv" "0,102"
+sh bin/create_board.sh ".." "64_NA6_94" "d" "bom_replace_hellen64_NA6_94-d.csv" "0,102"
 
